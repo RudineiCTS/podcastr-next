a1ccbb60ab724e5cc44bd40d3cffcaef490f6133
Rudinei Costa <costaaugustodev@gmail.com>
2021-05-08 14:01:20 -0300

    refactor: static pages dinamic

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -62,8 +62,22 @@ export default function Episodes({episodes}: episodesProps){
 }
 
 export const getStaticPaths: GetStaticPaths = async() =>{
+  const { data } = await api.get('episodes',{
+    params:{
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+  const paths = data.map(episodes =>{
+    return {
+      params:{
+        slug: episodes.id,
+      }
+    }
+  })
   return {
-    paths:[],
+    paths,
     fallback: 'blocking'
   }
 }
